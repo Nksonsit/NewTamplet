5b9b7ae8c65721272fe834ccdc1c0ae4a14d91b0
ishan <ishannsoni@gmail.com>
2018-10-09 18:43:19 +0530

    initial commit

--- app/src/main/java/com/drkeironbrown/lifecoach/ui/CategoriesActivity.java
+++ app/src/main/java/com/drkeironbrown/lifecoach/ui/CategoriesActivity.java
@@ -1,8 +1,6 @@
 package com.drkeironbrown.lifecoach.ui;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,9 +10,19 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import com.braintreepayments.api.BraintreeFragment;
+import com.braintreepayments.api.DataCollector;
+import com.braintreepayments.api.exceptions.InvalidArgumentException;
+import com.braintreepayments.api.interfaces.BraintreeCancelListener;
+import com.braintreepayments.api.interfaces.BraintreeErrorListener;
+import com.braintreepayments.api.interfaces.BraintreeResponseListener;
+import com.braintreepayments.api.interfaces.ConfigurationListener;
+import com.braintreepayments.api.interfaces.PaymentMethodNonceCreatedListener;
+import com.braintreepayments.api.models.Configuration;
+import com.braintreepayments.api.models.PayPalAccountNonce;
+import com.braintreepayments.api.models.PaymentMethodNonce;
+import com.braintreepayments.api.models.PostalAddress;
 import com.drkeironbrown.lifecoach.R;
 import com.drkeironbrown.lifecoach.adapter.CategoryAdapter;
 import com.drkeironbrown.lifecoach.api.RestClient;
@@ -23,33 +31,32 @@ import com.drkeironbrown.lifecoach.custom.TfTextView;
 import com.drkeironbrown.lifecoach.helper.Functions;
 import com.drkeironbrown.lifecoach.model.BaseResponse;
 import com.drkeironbrown.lifecoach.model.Category;
-import com.paypal.android.sdk.payments.PayPalAuthorization;
-import com.paypal.android.sdk.payments.PayPalConfiguration;
-import com.paypal.android.sdk.payments.PayPalFuturePaymentActivity;
-import com.paypal.android.sdk.payments.PayPalItem;
-import com.paypal.android.sdk.payments.PayPalOAuthScopes;
-import com.paypal.android.sdk.payments.PayPalPayment;
-import com.paypal.android.sdk.payments.PayPalPaymentDetails;
-import com.paypal.android.sdk.payments.PayPalProfileSharingActivity;
-import com.paypal.android.sdk.payments.PayPalService;
-import com.paypal.android.sdk.payments.PaymentActivity;
-import com.paypal.android.sdk.payments.PaymentConfirmation;
-import com.paypal.android.sdk.payments.ShippingAddress;
 
-import org.json.JSONException;
-
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class CategoriesActivity extends AppCompatActivity {
+public class CategoriesActivity extends AppCompatActivity implements ConfigurationListener,
+        PaymentMethodNonceCreatedListener, BraintreeErrorListener, BraintreeCancelListener {
+
+
+    static final String EXTRA_PAYMENT_RESULT = "payment_result";
+    static final String EXTRA_DEVICE_DATA = "device_data";
+    static final String EXTRA_COLLECT_DEVICE_DATA = "collect_device_data";
+    static final String EXTRA_ANDROID_PAY_CART = "android_pay_cart";
+
+    private static final String EXTRA_AUTHORIZATION = "com.braintreepayments.demo.EXTRA_AUTHORIZATION";
+    private static final String EXTRA_CUSTOMER_ID = "com.braintreepayments.demo.EXTRA_CUSTOMER_ID";
+
+    protected BraintreeFragment mBraintreeFragment;
+
+    private boolean mActionBarSetup;
+    private String mDeviceData;
+    private int selectedPos = -1;
+
 
     private android.widget.ImageView imgBack;
     private com.drkeironbrown.lifecoach.custom.TfTextView txtTitle;
@@ -76,7 +83,7 @@ public class CategoriesActivity extends AppCompatActivity {
         adapter = new CategoryAdapter(this, list, new CategoryAdapter.OnBuyClick() {
             @Override
             public void onBuyClick(int pos) {
-
+                selectedPos = pos;
             }
         });
         rvCategories.setAdapter(adapter);
@@ -125,10 +132,124 @@ public class CategoriesActivity extends AppCompatActivity {
             txtEmpty.setVisibility(View.VISIBLE);
             txtEmpty.setText(R.string.check_internet);
         }
+
+
+        try {
+            mBraintreeFragment = BraintreeFragment.newInstance(this, "sandbox_bsz8s3fp_225qyv663y373339");
+//            mBraintreeFragment = BraintreeFragment.newInstance(this, "sandbox_kswjqspg_b5qng8tnvn3sc48k");
+//            mBraintreeFragment = BraintreeFragment.newInstance(this, "sandbox_wvmtjryp_csrx6bnvrd78hyw9");
+        } catch (InvalidArgumentException e) {
+            onError(e);
+        }
+
+
     }
 
     @Override
     public void onBackPressed() {
         Functions.fireIntent(this, false);
+    }
+
+
+    @Override
+    public void onCancel(int requestCode) {
+
+        Log.e(getClass().getSimpleName(), "Cancel received: " + requestCode);
+    }
+
+    @Override
+    public void onError(Exception error) {
+        Log.e(getClass().getSimpleName(), "Error received (" + error.getClass() + "): " + error.getMessage());
+        Log.e(getClass().getSimpleName(), error.toString());
+    }
+
+    @Override
+    public void onConfigurationFetched(Configuration configuration) {
+        DataCollector.collectDeviceData(mBraintreeFragment, new BraintreeResponseListener<String>() {
+            @Override
+            public void onResponse(String deviceData) {
+                mDeviceData = deviceData;
+            }
+        });
+
+    }
+
+    @Override
+    public void onPaymentMethodNonceCreated(PaymentMethodNonce paymentMethodNonce) {
+
+        Log.e(getClass().getSimpleName(), "Payment Method Nonce received: " + paymentMethodNonce.getTypeLabel());
+
+        Intent intent = new Intent()
+                .putExtra(CategoriesActivity.EXTRA_PAYMENT_RESULT, paymentMethodNonce)
+                .putExtra(CategoriesActivity.EXTRA_DEVICE_DATA, mDeviceData);
+//        setResult(RESULT_OK, intent);
+//        finish();
+
+        list.get(selectedPos).setCategoryPrice("0");
+        adapter.setDataList(list);
+    }
+
+   /* private PayPalRequest getPayPalRequest(@Nullable String amount) {
+        PayPalRequest request = new PayPalRequest(amount);
+
+        request.displayName(Functions.getPayPalDisplayName(this));
+
+        String landingPageType = Functions.getPayPalLandingPageType(this);
+        if (getString(R.string.paypal_landing_page_type_billing).equals(landingPageType)) {
+            request.landingPageType(PayPalRequest.LANDING_PAGE_TYPE_BILLING);
+        } else if (getString(R.string.paypal_landing_page_type_login).equals(landingPageType)) {
+            request.landingPageType(PayPalRequest.LANDING_PAGE_TYPE_LOGIN);
+        }
+
+        String intentType = Functions.getPayPalIntentType(this);
+        if (intentType.equals(getString(R.string.paypal_intent_authorize))) {
+            request.intent(PayPalRequest.INTENT_AUTHORIZE);
+        } else if (intentType.equals(getString(R.string.paypal_intent_order))) {
+            request.intent(PayPalRequest.INTENT_ORDER);
+        } else if (intentType.equals(getString(R.string.paypal_intent_sale))) {
+            request.intent(PayPalRequest.INTENT_SALE);
+        }
+
+        if (Functions.isPayPalUseractionCommitEnabled(this)) {
+            request.userAction(PayPalRequest.USER_ACTION_COMMIT);
+        }
+
+        if (Functions.isPayPalCreditOffered(this)) {
+            request.offerCredit(true);
+        }
+
+        if (Functions.usePayPalAddressOverride(this)) {
+            request.shippingAddressOverride(new PostalAddress()
+                    .recipientName("Brian Tree")
+                    .streetAddress("123 Fake Street")
+                    .extendedAddress("Floor A")
+                    .locality("San Francisco")
+                    .region("CA")
+                    .countryCodeAlpha2("US")
+            );
+        }
+
+        return request;
+    }*/
+
+    public static String getDisplayString(PayPalAccountNonce nonce) {
+        return "First name: " + nonce.getFirstName() + "\n" +
+                "Last name: " + nonce.getLastName() + "\n" +
+                "Email: " + nonce.getEmail() + "\n" +
+                "Phone: " + nonce.getPhone() + "\n" +
+                "Payer id: " + nonce.getPayerId() + "\n" +
+                "Client metadata id: " + nonce.getClientMetadataId() + "\n" +
+                "Billing address: " + formatAddress(nonce.getBillingAddress()) + "\n" +
+                "Shipping address: " + formatAddress(nonce.getShippingAddress());
+    }
+
+    private static String formatAddress(PostalAddress address) {
+        return address.getRecipientName() + " " +
+                address.getStreetAddress() + " " +
+                address.getExtendedAddress() + " " +
+                address.getLocality() + " " +
+                address.getRegion() + " " +
+                address.getPostalCode() + " " +
+                address.getCountryCodeAlpha2();
     }
 }
